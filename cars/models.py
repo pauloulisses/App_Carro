1e1a8df15270f7c1bb2374aae93dd23d6de4e019
paulo <pauloulisses1212@gmail.com>
2024-10-07 23:23:41 -0300

    criação dos carros no BD

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # aqui onde vai escrever nosso modelos as tabelas do BD
+
+class Car(models.Model):
+
+    id = models.AutoField(primary_key=True) 
+    model = models.CharField(max_length=200) # modelo do carro
+    brand = models.CharField(max_length=200) # marca do carro
+    factory_year = models.IntegerField(blank=True, null=True) # ano de fabricação
+    model_year = models.IntegerField(blank=True, null=True) # ano modelo do carro
+    value = models.FloatField(blank=True, null=True) # valor do carro
+
+
+
