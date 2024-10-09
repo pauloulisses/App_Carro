68e5f3069a3609654e5c67145da93b48ab53a290
paulo <pauloulisses1212@gmail.com>
2024-10-08 22:31:44 -0300

    tapletes

--- cars/models.py
+++ cars/models.py
@@ -2,14 +2,24 @@ from django.db import models
 
 # aqui onde vai escrever nosso modelos as tabelas do BD
 
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 class Car(models.Model):
 
     id = models.AutoField(primary_key=True) 
     model = models.CharField(max_length=200) # modelo do carro
-    brand = models.CharField(max_length=200) # marca do carro
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand') # marca do carro
     factory_year = models.IntegerField(blank=True, null=True) # ano de fabricação
     model_year = models.IntegerField(blank=True, null=True) # ano modelo do carro
+    plate = models.CharField(max_length=10, blank=True, null=True) # placa dos carros
     value = models.FloatField(blank=True, null=True) # valor do carro
+    photo = models.ImageField(upload_to='cars/',blank=True, null=True) # manipulação de imagens 
 
-
+    def __str__(self):
+        return self.model # aqui para retorna o modelo do carro
 
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+
+from django.http import HttpResponse
 
 # As views no Django processam requisições HTTP e retornam respostas ao usuário, conectando dados e templates.
+
+def cars_view(request):
+    html = '''
+         <html>
+             <head>
+                  <title>Meus Carros</title>
+             </head>
+             <body>
+                  <h1>Carros do Ulisses</h1>
+                  <h3>Só Carro Top!</h3>
+             </body>
+                 
+           
+         </html>
+    '''
+    return HttpResponse(html)
